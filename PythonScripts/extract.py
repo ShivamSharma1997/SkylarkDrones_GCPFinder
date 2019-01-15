d9a15fe7a81f1daf5e847885c2c89d2723fafd50
Shivam <15UCC035@lnmiit.ac.in>
2019-01-15 17:11:48 +0530

    Update

--- PythonScripts/extract.py
+++ PythonScripts/extract.py
@@ -3,15 +3,30 @@ import os
 from sift import SIFT
 from tqdm import tqdm
 
+###################### DEFINING DATA PATH ######################
+
+'''
+
+-> Please change the following paths.
+
+-> The "writeImg" option in sift.getPoints is to save images as processed
+   by the SIFT function. Please update the "writeDir" option to change the path
+   to save the processed images.
+
+'''
+
 dataDir = '../data'
 train_fldr = 'train_imgs1'
+resultFile = 'results.csv'
+
+###################### GETTING POINTS AND SAVING RESULTS ######################
 
 imgNames = os.listdir('{}/{}'.format(dataDir, train_fldr))
 
 sift = SIFT(alphaMin = 0.2, alphaMax = 0.45)
-saveFile = open('../data/results.csv', 'w')
+saveFile = open('{}/{}'.format(dataDir, resultFile), 'w')
 
-for name in tqdm(imgNames):
+for name in tqdm(imgNames[0:1]):
     matchPoints = sift.getPoints('{}/{}/{}'.format(dataDir, train_fldr, name),
                                  '{}/templates'.format(dataDir), writeImg=True,
                                  writeDir='../data/results/')
